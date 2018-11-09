594c5833a18acaacdef602b1f69e751347075b87
joecosta117 <joecosta117@gmail.com>
2018-11-09 13:40:40 -0500

    added buttons for all dice and they create an alert with the result

--- screens/RollingScreen.js
+++ screens/RollingScreen.js
@@ -16,6 +16,10 @@ export default class RollingScreen extends React.Component {
       // <Text>Hero Roller!</Text>
       <View>
         <Button
+          onPress={() => {
+            let result = d20.roll('1d100');
+            Alert.alert(`You rolled a ${result}!`)
+          }}
           raised
           title='ROLL d100!' />
         <Button
@@ -26,18 +30,38 @@ export default class RollingScreen extends React.Component {
           raised
           title='ROLL d20!' />
         <Button
+          onPress={() => {
+            let result = d20.roll('1d12');
+            Alert.alert(`You rolled a ${result}!`)
+          }}
           raised
           title='ROLL d12!' />
         <Button
+          onPress={() => {
+            let result = d20.roll('1d10');
+            Alert.alert(`You rolled a ${result}!`)
+          }}
           raised
           title='ROLL d10!' />
         <Button
+          onPress={() => {
+            let result = d20.roll('1d8');
+            Alert.alert(`You rolled a ${result}!`)
+          }}
           raised
           title='ROLL d8!' />
         <Button
+          onPress={() => {
+            let result = d20.roll('1d6');
+            Alert.alert(`You rolled a ${result}!`)
+          }}
           raised
           title='ROLL d6!' />
         <Button
+          onPress={() => {
+            let result = d20.roll('1d4');
+            Alert.alert(`You rolled a ${result}!`)
+          }}
           raised
           title='ROLL d4!' />
           <Text>Your roll result is:</Text>
